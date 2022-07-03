af2e42b328d9cdda366b69df6be765a965d58d19
Yotam Granov <63986847+Yomaster10@users.noreply.github.com>
2022-07-03 16:32:34 +0300

    Updating answers

--- hw3/answers.py
+++ hw3/answers.py
@@ -49,26 +49,20 @@ def part1_generation_params():
 
 part1_q1 = r"""
 **Your answer:**
-Question: Why do we split the corpus into sequences instead of training on the whole text?
 
-Answer:
-
+The general idea is similar to the reason why mini-batch gradient descent is often more optimal than batch gradient descent: this method is more computationally efficient and allows the model to "see" more data, causing it to learn more effectively. Beyond that, this method allows us to conduct parallel computations on "batches" (sequences), which can help reduce computation time further and boost the learning process. This grants us the ability to use less deep networks for our problem, and so we can avoid vanishing or exploding gradients more efficiently.
 """
 
 part1_q2 = r"""
 **Your answer:**
-Question: How is it possible that the generated text clearly shows memory longer than the sequence length?
 
-Answer:
-
+The generated text seems to have a memory longer than the sequence length due to the fact that our model uses hidden states to memorize longer text than the sequence length, as well as the fact that our model is "seeing" the data sequentially and thus relies on information from previous "batches". Thus, it is able to produce longer sentences.
 """
 
 part1_q3 = r"""
 **Your answer:**
-Question: Why are we not shuffling the order of batches when training?
 
-Answer:
-
+Our model here is a recurrent neural network (RNN), meaning that it learns from the training data in a sequential manner and so every training step where the model is learning from a "batch" of data will be affected by the batches that precede it, and even moreso it will be affected by their order. If we were to shuffle the order of the batches when training our RNN, we would not allow it to properly learn the "flow" of human language, since the model can no longer view the training text in the correct order. It is crucial in fact that we don't shuffle the order of the batches during training, so that our model will be able to learn correctly and efficiently.
 """
 
 part1_q4 = r"""
@@ -81,6 +75,10 @@ Questions:
 
 Answers:
 **(1)**
+
+Yuval: Compared to the default of 1.0, we lower the temperature for sampling in order to allow to model to choose a “better” word (from a score/value perspective) – by lowering the temperature we increase the difference between different probabilities for different words, therefore allowing a better choice of the model.
+
+Tomer: Lower temperature results a less uniform distribution of next likely letters. Therefore the model is likely  to pick letters which will create coherent words and grammatically correct sentences which closely resemble data it previously encountered during its training phase.
 
 **(2)** When we raise the temperature too high (for example to $T=10,000$), the text we generate becomes absolute gibberish. This is because we are essentially evening out the probabilities for every character, giving them all a much mroe similar weight. Thus, the model has lost its ability to generate reasonable sentence structure and spelling, and the choice of characters is essentially random. Here the analogy to temperature in the thermodynamic sense becomes more clear: high temperature causes high entropy, meaning that there is disorder and chaos in the system. Our generated text here is super disordered and chaotic, and very little real information can be obtained from it.
 
@@ -108,9 +106,9 @@ def part2_vae_hyperparams():
     hypers['batch_size'] = 128
     hypers['h_dim'] = 128
     hypers['z_dim'] = 32
-    hypers['x_sigma2'] = 0.0001
-    hypers['learn_rate'] = 0.0002
-    hypers['betas'] = (0.5, 0.999)
+    hypers['x_sigma2'] = 0.0002
+    hypers['learn_rate'] = 0.0004
+    hypers['betas'] = (0.5, 0.599)
     # ========================
     return hypers
 
@@ -184,17 +182,16 @@ def part3_gan_hyperparams():
         discriminator_optimizer=dict(
             type="",  # Any name in nn.optim like SGD, Adam
             lr=0.0,
-            # You an add extra args for the optimizer here
+            betas=(0.0, 0.0)
         ),
         generator_optimizer=dict(
             type="",  # Any name in nn.optim like SGD, Adam
             lr=0.0,
-            # You an add extra args for the optimizer here
+            betas=(0.0, 0.0)
         ),
     )
     # TODO: Tweak the hyperparameters to train your GAN.
     # ====== YOUR CODE: ======
-
     hypers['batch_size'] = 128
     hypers['z_dim'] = 3
     hypers['data_label'] = 1
@@ -210,29 +207,23 @@ def part3_gan_hyperparams():
 
 
 part3_q1 = r"""
-In the case of GANs, we want to keep the gradients during training for the generator and discriminator separately because they are trained separately.
-When training the generator we maintain gradients of the generator and not the discriminator and vice versa.
+**Your answer:**
+
+In the case of GANs, we want to keep the gradients during training for the generator and discriminator separately because they are trained separately. When training the generator we maintain gradients of the generator and not the discriminator and vice versa.
 """
 
 part3_q2 = r"""
-1.	**NO**. The entire model loss depends on both the generator loss and the discriminator loss and thus the
-loss might still be too high even if the generator loss went under some threshold. For example if the discriminator
-loss is extremely high, it could cause the generator loss to be low even though the images that it generates are bad.
+**Your answer:**
 
-2.	It could mean that generator’s learning speed is higher than the discriminator’s and that the discriminator is not learning fast enough, to keep up with the generator’s learning pace.
+**(1)**	*NO*. The entire model loss depends on both the generator loss and the discriminator loss and thus the loss might still be too high even if the generator loss went under some threshold. For example if the discriminator loss is extremely high, it could cause the generator loss to be low even though the images that it generates are bad.
 
+**(2)**	It could mean that generator’s learning speed is higher than the discriminator’s and that the discriminator is not learning fast enough, to keep up with the generator’s learning pace.
 """
 
 part3_q3 = r"""
-The main difference between the images generated from the two models seems to be the colors and the features learned by
-each model.
-It seems that the VAE model results have more details in the face area, while the GAN results have more details in the body area and the colors of the background.
-Because the architectures are different it seems that the features learned are also different. it could be the case that the features learned to fool the discriminator
-are not necessarily the sharpnes of the face, thus causing the images to appear blurry in that area.
-It also seems that the VAE model did a better job in learning features that allow us (as humans) to recognize the image as
-George Bush.
+**Your answer:**
 
+The main difference between the images generated from the two models seems to be the colors and the features learned by each model. It seems that the VAE model results have more details in the face area, while the GAN results have more details in the body area and the colors of the background. Because the architectures are different it seems that the features learned are also different. it could be the case that the features learned to fool the discriminator are not necessarily the sharpnes of the face, thus causing the images to appear blurry in that area. It also seems that the VAE model did a better job in learning features that allow us (as humans) to recognize the image as George Bush.
 """
-
 
 # ==============
